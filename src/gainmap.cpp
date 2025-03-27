dd438da4e3cdd9714a2aa9fe760f6630549149e5
Mathieu Tuli <tuli.mathieu@gmail.com>
2025-03-27 15:09:39 -0400

    UPdates to mean

--- src/gainmap.cpp
+++ src/gainmap.cpp
@@ -173,8 +173,12 @@ void HDRToGainMap(const std::unique_ptr<imageops::Image> &hdr_image,
     std::cout << "HDR " << clip_percentile
               << "th-percentile clip value: " << clip_value << std::endl;
 
-    std::vector<float> sdr_values;
-    sdr_values.reserve(width * height * channels);
+    std::vector<float> sdr_values1;
+    std::vector<float> sdr_values2;
+    std::vector<float> sdr_values3;
+    sdr_values1.reserve(width * height * channels);
+    sdr_values2.reserve(width * height * channels);
+    sdr_values3.reserve(width * height * channels);
     for (size_t i = 0; i < hdr_linear_image.size(); i++) {
         colorspace::Color hdr_rgb = hdr_linear_image[i];
         colorspace::Color sdr_rgb = sdr_gammut_conv(hdr_rgb);
@@ -196,14 +200,21 @@ void HDRToGainMap(const std::unique_ptr<imageops::Image> &hdr_image,
         srgb_gamma.r = static_cast<float>(r) / 255.f;
         srgb_gamma.g = static_cast<float>(g) / 255.f;
         srgb_gamma.b = static_cast<float>(b) / 255.f;
+        sdr_values1.push_back(sdr_rgb.r);
+        sdr_values1.push_back(sdr_rgb.g);
+        sdr_values1.push_back(sdr_rgb.b);
 
         sdr_rgb = sdr_inv_oetf(srgb_gamma);
 
-        sdr_values.push_back(sdr_rgb.r);
-        sdr_values.push_back(sdr_rgb.g);
-        sdr_values.push_back(sdr_rgb.b);
+        sdr_values2.push_back(sdr_rgb.r);
+        sdr_values2.push_back(sdr_rgb.g);
+        sdr_values2.push_back(sdr_rgb.b);
 
         colorspace::Color sdr_rgb_bt2100 = sdr_hdr_gamut_conv(sdr_rgb);
+
+        sdr_values3.push_back(sdr_rgb_bt2100.r);
+        sdr_values3.push_back(sdr_rgb_bt2100.g);
+        sdr_values3.push_back(sdr_rgb_bt2100.b);
 
         float hdr_y_nits, sdr_y_nits;
 
@@ -217,9 +228,15 @@ void HDRToGainMap(const std::unique_ptr<imageops::Image> &hdr_image,
         max_gain = std::max(gain, max_gain);
         gainmap.push_back(gain);
     }
-    float average = std::accumulate(sdr_values.begin(), sdr_values.end(), 0.0) /
-                    sdr_values.size();
-    std::cout << "The average is " << average << std::endl;
+    float average1 = std::accumulate(sdr_values1.begin(), sdr_values1.end(), 0.0) /
+                    sdr_values1.size();
+    float average2 = std::accumulate(sdr_values2.begin(), sdr_values2.end(), 0.0) /
+                    sdr_values2.size();
+    float average3 = std::accumulate(sdr_values3.begin(), sdr_values3.end(), 0.0) /
+                    sdr_values3.size();
+    std::cout << "The average1 is " << average1<< std::endl;
+    std::cout << "The average2 is " << average2<< std::endl;
+    std::cout << "The average3 is " << average3<< std::endl;
     std::cout << "Gainmap computed." << std::endl;
 
     // generate map
